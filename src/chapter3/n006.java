25b9f43a427db60206e1beb00a6e9088d0a84abf
ybwi0912 <ybwi0912@naver.com>
2023-02-07 21:55:05 +0900

    23.02.07

---
+++ src/chapter3/n006.java
@@ -0,0 +1,31 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class n006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int start_index = 1;
+        int end_index = 1;
+        int count = 1;
+        int sum = 1;
+
+        while (end_index != N){
+            if(sum>N){
+                sum -= start_index;
+                start_index++;
+            }
+            else if (sum<N){
+                end_index++;
+                sum += end_index;
+            }
+            else{
+                end_index++;
+                sum += end_index;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
