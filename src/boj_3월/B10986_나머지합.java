708d662d24d80df0e216b2345f0c760396d6ce1d
ybwi0912 <ybwi0912@naver.com>
2023-03-12 15:39:21 +0900

    23.03.12

--- src/boj_3월/B10986_나머지합.java
+++ src/boj_3월/B10986_나머지합.java
@@ -13,26 +13,27 @@ public class B10986_나머지합 {
         int N = Integer.parseInt(token.nextToken()); // 수의 개수 N개
         int M = Integer.parseInt(token.nextToken()); // 합이 M으로 나누어떨어지는 ..
 
-        int[] arr = new int[N];
-        int[] sumArr = new int[N];
+        long[] arr = new long[N];
+        long ans = 0;
+        long count[] = new long[M];
 
         token = new StringTokenizer(bf.readLine());
         arr[0] = Integer.parseInt(token.nextToken());
-        sumArr[0] = arr[0];
 
-        for(int i=1; i<N; i++) {
-            arr[i] = Integer.parseInt(token.nextToken()); // N개의 수
-            sumArr[i] = sumArr[i-1] + arr[i];
-        }
-        int ans = 0;
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<i; j++) {
-                if((sumArr[i] - sumArr[j])%M==0)
-                    ans++;
-            }
+        for(int i=1; i<N; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(token.nextToken());
         }
 
+        for(int i=0; i<N; i++){
+            int remainder = (int)(arr[i] % M);
+            if(remainder==0) ans++;
+            count[remainder]++;
+        }
+
+        for(int i=0; i<M; i++){
+            ans += count[i] * (count[i]-1) / 2;
+        }
         System.out.println(ans);
+
     }
 }
