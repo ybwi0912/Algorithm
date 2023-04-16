ef56ba41d0ceb1c7a73710e01704a61d92970911
ybwi0912 <ybwi0912@naver.com>
2023-04-16 21:29:10 +0900

    23.04.16 프로그래머스 42748 - added code review

--- src/prog/P42748_K번째수.java
+++ src/prog/P42748_K번째수.java
@@ -23,23 +23,38 @@ public class P42748_K번째수 {
     static class Solution {
         public static int[] solution(int[] array, int[][] commands) {
             int count = 0;
-            for(int[] a : commands){
-                count++;
-            }
-
-            int[] answer = new int[count];
+//            for(int[] a : commands){
+//                count++;
+//            }
+//            int[] answer = new int[count];
+            int[] answer = new int[commands.length];
+            /*
+            * for문 자체도, for문 안에서 생성되는 a 변수도 메모리와 시간을 잡아먹기 때문에 .length 메소드 활용이 나아 보인다
+            * */
             count = 0;
 
             for(int[] comArr : commands){ // 각 연산
-                int len = comArr[1] - comArr[0] + 1;
-                int[] newArr = new int[len]; // 각 연산마다 새로 생기는 배열
-                for(int i=0; i<len; i++){
-                    newArr[i] = array[comArr[0]-1+i];
-                }
+//                int len = comArr[1] - comArr[0] + 1;
+//                int[] newArr = new int[len]; // 각 연산마다 새로 생기는 배열
+//                for(int i=0; i<len; i++){
+//                    newArr[i] = array[comArr[0]-1+i];
+//                }
+                int[] newArr = Arrays.copyOfRange(array, comArr[0]-1, comArr[1]);
+                /*
+                * 어차피 Arrays를 import할 거면 .copyOfRange 메소드를 활용하는 것이 속도도 빠르고
+                * 매번 크기(len)을 계산하는 번거로움도 줄일 수 있다
+                * copyOfRange(원본 배열, 시작 인덱스, 끝 인덱스) - 원본 배열이 그대로 유지된다
+                * */
+
                 Arrays.sort(newArr);
-                int i = newArr[comArr[2]-1];
-                answer[count] = i;
-                count++;
+//                int i = newArr[comArr[2]-1];
+//                answer[count] = i;
+//                count++;
+                answer[count++] = newArr[comArr[2]-1];
+                /*
+                * 불필요한 i 변수 선언 대신 한 줄로 줄일 수 있다
+                * */
+
             }
             return answer;
         }
