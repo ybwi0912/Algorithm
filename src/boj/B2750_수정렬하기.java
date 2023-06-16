829ac4a48281d02a29b011722efa8e25d3209868
ybwi0912 <ybwi0912@naver.com>
2023-06-16 16:47:02 +0900

    23.06.16 BOJ 2750 (Bubble Sort)

---
+++ src/boj/B2750_수정렬하기.java
@@ -0,0 +1,46 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 2023-06-16
+ * Sorting Algorithm
+ * */
+
+public class B2750_수정렬하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 수의 개수
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+        // input
+
+        bubbleSort(arr);
+        // operation
+
+        for(int i : arr){
+            System.out.println(i);
+        }
+        // output
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void bubbleSort(int[] arr){
+        int len = arr.length;
+        for(int i=0; i<len-1; i++){
+            for(int j=0; j<len-1-i; j++){
+                if(arr[j]>arr[j+1])
+                    swap(arr, j, j+1);
+            }
+        }
+    }
+}
