f94ea28adaad5b4c04b3aad5145739efe4e23fef
ybwi0912 <ybwi0912@naver.com>
2023-12-28 22:53:33 +0900

    23.12.28 BOJ 1927. 최소 힙(heappush 구현 시도)

--- src/boj/B1927_최소힙.py
+++ src/boj/B1927_최소힙.py
@@ -1,6 +1,19 @@
 import heapq
 import sys
 
+def heappush(heap, data):
+    heap.append(data)
+
+    now = len(heap) - 1
+
+    while now > 0:
+        parent = (now - 1) // 2
+        if heap[parent] > heap[now]:
+            heap[parent], heap[now] = heap[now], heap[parent]
+            now = parent
+        else:
+            break
+
 n = int(sys.stdin.readline())
 heap = []
 
