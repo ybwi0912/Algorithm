6ab36f3d567efae2e8dd18d0c4887a0e3dbef049
ybwi0912 <ybwi0912@naver.com>
2024-01-05 21:37:05 +0900

    24.01.05 BOJ 11050. 이항 계수 1

---
+++ src/boj/B11050_이항계수1.py
@@ -0,0 +1,13 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+d = [[0 for i in range(n+1)] for j in range(n+1)]
+for i in range(n+1):
+    d[i][1] = i
+    d[i][0] = 1
+    d[i][i] = 1
+
+for i in range(2, n+1):
+    for j in range(1, i):
+        d[i][j] = d[i-1][j] + d[i-1][j-1]
+
+print(d[n][k])
